adb09a7d85ffdbd82c8abec431a80ca77e7a61fa
zakarie <zakarie.abdi@student.moringaschool.com>
2025-11-27 21:42:32 +0300

    Add Axios JWT interceptor to fix 422 errors

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,19 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://psu-certificate-verification-1.onrender.com'
+  baseURL: 'https://psu-certificate-verification-1.onrender.com/api'
 })
 
-console.log('API URL:', 'https://psu-certificate-verification-1.onrender.com')
+// Attach JWT token automatically for protected routes
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
 
 export default api
